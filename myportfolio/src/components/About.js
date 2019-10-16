faeb8ba335eb02783f19e2a8028390ca11cabd36
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-16 16:27:13 -0700

    Added gsap to landing page

--- myportfolio/src/components/About.js
+++ myportfolio/src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <>
             <div className='aboutCon'>
                 <img src={personalPic} alt='personalPic' className='personalPic' />
-                <p className='aboutText'>Hi! I'm Gavin Dreyer and I am a Califonia based fullstack web developer. The programming languages I am most proficient at include HTML, CSS, and Javascript. Java coming soon{String.fromCharCode(8482)}. Most of my time is spent doing what I love, building Web Apps with React! When I'm not working on a project, you'll most likely find me watching Baseball! Go SF Giants! ⚾️
+                <p className='aboutText'>Hi! I'm Gavin Dreyer and I am a Califonia based fullstack web developer. The programming languages I am most proficient at include HTML, CSS, and Javascript. Java coming soon{String.fromCharCode(8482)}. Most of my time is spent doing what I love, building Web Apps with React! When I'm not working on a project, you'll most likely find me at the beach or watching Baseball! Go SF Giants! ⚾️
                 </p>
             </div>
             
--- myportfolio/src/components/LandingPage.js
+++ myportfolio/src/components/LandingPage.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import {TweenMax, Linear} from 'gsap';
 import { FaGithubSquare } from 'react-icons/fa'
 
 
 
 const LandingPage = () => {
+    let logoElement = useRef(null) 
+    let textElement = useRef(null)
+
+    useEffect(() => {
+        console.log(logoElement)
+        TweenMax.from(
+            logoElement,
+            2,
+            {x:300, opacity:0, scale:0.5},
+        )
+        TweenMax.from(
+            textElement,
+            2,
+            {x:-300, opacity:0, scale:0.5},
+        )
+    }, []);
+
+    function scaleUp() {
+        TweenMax.to(logoElement, 1, {
+            scale: 1.25,
+            ease: Linear.ease
+        });
+    }
+
+    function scaleDown() {
+        TweenMax.to(logoElement, 1, {
+            scale: 0.75
+        });
+    }
+
     return(
         <div>
             <div className='personalCon'>
                 <div className='flexCon'>
-                    <a href='https://github.com/gavin-dreyer'><FaGithubSquare className='githubIcon' /></a>
-                    <p>Welcome to my Website 😃</p>
+                    <a href='https://github.com/gavin-dreyer'
+                    onMouseEnter={scaleUp} 
+                    onMouseLeave={scaleDown}
+                    ref={element => {logoElement = element}} 
+                    ><FaGithubSquare className='githubIcon' /></a>
+                    <p
+                    ref={element => {textElement = element}}>Welcome to my Website 😃</p>
                 </div>
             </div>
         </div>
