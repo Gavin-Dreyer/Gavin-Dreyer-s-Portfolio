58cdc342a10786dc37ec89cc03314fdfea52cd6b
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-16 19:09:17 -0700

    changed personalPic to background image

--- myportfolio/src/components/About.js
+++ myportfolio/src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <>
             <div className='aboutCon'>
-                <img src={personalPic} alt='personalPic' className='personalPic' />
+                <div className='personalPic'></div>
                 <p className='aboutText'>Hi! I'm Gavin Dreyer and I am a Califonia based fullstack web developer. The programming languages I am most proficient at include HTML, CSS, and Javascript. Java coming soon{String.fromCharCode(8482)}. Most of my time is spent doing what I love, building Web Apps with React! When I'm not working on a project, you'll most likely find me at the beach or watching Baseball! Go SF Giants! ⚾️
                 </p>
             </div>
