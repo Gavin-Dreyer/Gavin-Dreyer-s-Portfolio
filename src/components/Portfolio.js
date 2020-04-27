16ba295157181396838882bfa00357505b27e321
Gavin Dreyer <gavindreyer1@gmail.com>
2020-04-27 13:30:22 -0700

    added a carousel to navigate through the projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,17 +8,40 @@ import Project3 from './projects/Project3';
 const Portfolio = () => {
 	const [project, setProject] = useState(1);
 
-	const navigate = () => {
-		{
-			project === 1 ? setProject(0) : setProject(1);
+	const navigateUp = () => {
+		if (project === 1) {
+			setProject(2);
+		} else if (project === 2) {
+			setProject(3);
+		} else if (project === 3) {
+			setProject(1);
+		}
+	};
+
+	const navigateDown = () => {
+		if (project === 3) {
+			setProject(2);
+		} else if (project === 2) {
+			setProject(1);
+		} else if (project === 1) {
+			setProject(3);
 		}
 	};
 
 	return (
-		<>
-			<button onClick={() => navigate()}>CLICK ME</button>
-			{project === 1 ? <Project3 /> : <Project2 />}
-		</>
+		<div className="portfolio">
+			<button className="arrowBtn" onClick={() => navigateUp()}>
+				{'⬅'}
+			</button>
+
+			{project === 1 ? <Project1 /> : ''}
+			{project === 2 ? <Project2 /> : ''}
+			{project === 3 ? <Project3 /> : ''}
+
+			<button className="arrowBtn" onClick={() => navigateDown()}>
+				{'⮕'}
+			</button>
+		</div>
 	);
 };
 
