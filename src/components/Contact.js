2e36a21b579760c3220c39569bdcb47d445bbb3d
Gavin Dreyer <gavindreyer1@gmail.com>
2020-04-29 12:42:52 -0700

    styling for contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const encode = data => {
 	return Object.keys(data)
@@ -28,44 +28,47 @@ const Contact = () => {
 	const handleChange = e =>
 		setContact({ ...contact, [e.target.name]: e.target.value });
 
-	console.log(contact);
 	return (
-		<div>
+		<div className="contact">
+			<div>If you'd like to connect, please send me a message!</div>
 			<form onSubmit={handleSubmit}>
 				<input type="hidden" name="form-name" value="contact" />
 				<p>
-					<label>
-						Your Name:{' '}
+					<label className="inputLabel">
 						<input
 							type="text"
 							name="name"
+							placeholder="Name"
 							value={contact.name}
 							onChange={handleChange}
+							className="contactInput"
 						/>
 					</label>
 				</p>
 				<p>
-					<label>
-						Your Email:{' '}
+					<label className="inputLabel">
 						<input
 							type="email"
 							name="email"
+							placeholder="Email"
 							value={contact.email}
 							onChange={handleChange}
+							className="contactInput"
 						/>
 					</label>
 				</p>
 				<p>
-					<label>
-						Message:{' '}
+					<label className="inputLabel">
 						<textarea
 							name="message"
+							placeholder="Message"
 							value={contact.message}
 							onChange={handleChange}
+							className="contactInputMessage"
 						/>
 					</label>
 				</p>
-				<p>
+				<p className="submitBtn">
 					<button type="submit">Send</button>
 				</p>
 			</form>
