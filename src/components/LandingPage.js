fe9a4f7543118e93c7a306be81df74f06f7eb4e1
Gavin Dreyer <gavindreyer1@gmail.com>
2020-05-27 22:30:56 -0700

    nav bar animation on load

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { TweenMax, Linear, Bounce, Back } from 'gsap';
+import { TweenMax, Back } from 'gsap';
 import { FaGithubSquare } from 'react-icons/fa';
 
 const LandingPage = () => {
@@ -31,34 +31,31 @@ const LandingPage = () => {
 	}
 
 	return (
-		<div>
-			<div className="personalCon">
-				<div className="flexCon">
-					<a
-						href="https://github.com/gavin-dreyer"
-						onMouseEnter={scaleUp}
-						onMouseLeave={scaleDown}
-						ref={element => {
-							logoElement = element;
-						}}
-					>
-						<FaGithubSquare className="githubIcon" />
-					</a>
-					<p
-						ref={element => {
-							textElement = element;
-						}}
-					>
-						Welcome to my site! I'm Gavin, a full stack web developer that
-						primarily works in React/Node with JavaScript, while also being
-						competent in working with Python. I am passionate about programming
-						because I love building things that help people improve themselves
-						or keep track of their data. You can check out my recent projects{' '}
-						<a href="https://gavindreyer.com/portfolio">here </a>and what I'm
-						currently working on in GitHub above.
-					</p>
-				</div>
-			</div>
+		<div className="flexCon">
+			<a
+				href="https://github.com/gavin-dreyer"
+				onMouseEnter={scaleUp}
+				onMouseLeave={scaleDown}
+				ref={element => {
+					logoElement = element;
+				}}
+			>
+				<FaGithubSquare className="githubIcon" />
+			</a>
+			<p
+				ref={element => {
+					textElement = element;
+				}}
+				id="aboutText"
+			>
+				{/* Welcome to my site! I'm Gavin, a full stack web developer that primarily
+				works in React/Node with JavaScript, while also being competent in
+				working with Python. I am passionate about programming because I love
+				building things that help people improve themselves or keep track of
+				their data. You can check out my recent projects{' '}
+				<a href="https://gavindreyer.com/portfolio">here </a>and what I'm
+				currently working on in GitHub above. */}
+			</p>
 		</div>
 	);
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,119 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { TweenMax, Back } from 'gsap';
 import { Link } from 'react-router-dom';
 
 const NavBar = props => {
+	let link1 = useRef(null);
+	let link2 = useRef(null);
+	let link3 = useRef(null);
+	let link4 = useRef(null);
+
+	const [l1Class, setL1Class] = useState('navBarLink1Load');
+	const [divClass, setDivClass] = useState('linkConLoad');
+	const [hidden, setHidden] = useState(true);
+	const [hidden1, setHidden1] = useState(false);
+
+	const transition = () => {
+		divClass === 'linkConLoad'
+			? setDivClass('linkCon')
+			: setDivClass('linkConLoad');
+
+		l1Class === 'navBarLink1Load'
+			? setL1Class('navBarLink1')
+			: setL1Class('navBarLink1Load');
+
+		setTimeout(() => {
+			hidden ? setHidden(false) : setHidden(true);
+
+			hidden1 ? setHidden1(false) : setHidden1(true);
+		}, 1400);
+	};
+
+	useEffect(() => {
+		transition();
+	}, []);
+
+	useEffect(() => {
+		if (!hidden) {
+			TweenMax.from(link1, 1, {
+				opacity: 0,
+				scale: 1,
+				delay: 0.4,
+				ease: Back.ease
+			});
+			TweenMax.from(link2, 1, {
+				opacity: 0,
+				scale: 1,
+				delay: 0.5,
+				ease: Back.ease
+			});
+			TweenMax.from(link3, 1, {
+				opacity: 0,
+				scale: 1,
+				delay: 0.6,
+				ease: Back.ease
+			});
+			TweenMax.from(link4, 1, {
+				opacity: 0,
+				scale: 1,
+				delay: 0.7,
+				ease: Back.ease
+			});
+		}
+	}, [hidden]);
+
 	return (
 		<div className="navBar">
-			<Link className="navBarLink1" to="/">
-				Home
-			</Link>
-			<Link className="navBarLink1" to="/about">
-				About
-			</Link>
-			<Link className="navBarLink2" to="/portfolio">
-				Portfolio
-			</Link>
-
-			<Link className="navBarLink2" to="/contact">
-				Contact
-			</Link>
+			{/* <button onClick={() => transition()}></button> */}
+			{hidden1 ? null : <div className={divClass + '1'}>Home</div>}
+			{hidden ? null : (
+				<Link
+					className={l1Class}
+					to="/"
+					// hidden={hidden}
+					ref={element => {
+						link1 = element;
+					}}
+				>
+					Home
+				</Link>
+			)}
+			{hidden1 ? null : <div className={divClass + '2'}>About</div>}
+			{hidden ? null : (
+				<Link
+					className="navBarLink1"
+					to="/about"
+					ref={element => {
+						link2 = element;
+					}}
+				>
+					About
+				</Link>
+			)}
+			{hidden1 ? null : <div className={divClass + '3'}>Portfolio</div>}
+			{hidden ? null : (
+				<Link
+					className="navBarLink2"
+					to="/portfolio"
+					ref={element => {
+						link3 = element;
+					}}
+				>
+					Portfolio
+				</Link>
+			)}
+			{hidden1 ? null : <div className={divClass + '4'}>Contact</div>}
+			{hidden ? null : (
+				<Link
+					className="navBarLink2"
+					to="/contact"
+					ref={element => {
+						link4 = element;
+					}}
+				>
+					Contact
+				</Link>
+			)}
 		</div>
 	);
 };
